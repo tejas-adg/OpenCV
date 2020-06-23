ff8a282cfb315fe32c870fef668de5969b0540fc
Tejas Adagoor Manjunatha <tejasam@vt.edu>
2020-06-23 11:37:14 +0530

    Added comments

--- Image_Resizing.py
+++ Image_Resizing.py
@@ -3,6 +3,8 @@ from Modified_Functions import Get_Image, Show_image, Get_Resized_Image
 
 img = Get_Image()
 
+# Another way to reduce an image is to use functions on the X-axis and Y-axis
+# Here, the fx and fy fulfil the purpose and we are reducing them by 50% or 0.5
 half_img = cv2.resize(img, (0, 0), fx = 0.5, fy = 0.5)
 Show_image("Half", half_img)
 
