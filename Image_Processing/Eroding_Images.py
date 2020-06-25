8df4655ccdd577824305f42e18a7bdef2aa3f241
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2020-06-25 23:16:46 +0530

    eroding images

--- Image_Processing/Eroding_Images.py
+++ Image_Processing/Eroding_Images.py
@@ -1,2 +1,3 @@
-import cv2
-from Modified_Functions import Get_Resizied_Image, Show_Image
+import os, sys, cv2
+sys.path.append(os.getcwd()[:os.getcwd().find(os.path.basename(os.getcwd())) - 1])
+from Modified_Functions import Show_image, Get_Image_Dir
