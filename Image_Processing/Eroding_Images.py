cd5f6a6bc4526cd27673eabc783d75e182e2ac90
Tejas Adagoor Manjunatha <tejas_adg@hotmail.com>
2020-06-26 20:51:38 +0530

    learnt eroding images

--- Image_Processing/Eroding_Images.py
+++ Image_Processing/Eroding_Images.py
@@ -2,4 +2,4 @@ import os, sys, cv2
 sys.path.append(os.getcwd()[:os.getcwd().find(os.path.basename(os.getcwd())) - 1])
 from Modified_Functions import Show_image, Get_Image_Dir
 
-# In here, we will erode an image away
+# The idea of eroding an image is just like soil erosion
